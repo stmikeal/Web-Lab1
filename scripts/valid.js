a97b244f7b564c7b8b8385920f8a10fe6d817410
Mike Steph <timetocook420@gmail.com>
2021-09-02 19:19:46 +0700

    final

--- scripts/valid.js
+++ scripts/valid.js
@@ -1,20 +1,97 @@
-var joke_number = 1000;
-var jokes = ['В Москве прошел конкурс на самое нелепое имя. <br>Победила заняла первое место.',
-	'- Знакомьтесь, это Вася. Он изучает эритроциты.<br>- Да, я изучаю эритроциты, мой отец изучал эритроциты, мой дед изучал эритроциты. Понимаете, эритроциты - это у нас в крови.',
-	'Купил мужик курицу, а она ему как раз.<br><img src="img/joke0.jpg" width="350">',
-	'Собака по кличке новость нашла в деревне закладку бомбы.<br>Новость быстро разлетелась по всей деревне.',
-	'<img src="img/joke1.jpg" width="350">',
-	'Как армяне, живущие в Италии, назвали своего сына?<br>-Леонарды',
-	'Заходит слепой в магазин и начинает раскручивать над головой собаку-поводыря.<br>– Что вы делаете, сэр?<br>– Я осматриваюсь.',
-	'Почему сиамские близнецы поссорились?<br>Потеряли общий язык.'];
-function joke() {
-	var number;
-	while(true) {
-		number = Math.floor(Math.random()*jokes.length);
-		if (number!=joke_number) {
-			joke_number = number;
-			break;
+function onlyDigits() {
+	var separator = this.dataset.separator;
+	var replaced = new RegExp('[^\\d\\'+separator+'\\-]', "g");
+	var regex = new RegExp('\\'+separator, "g");
+	this.value = this.value.replace(replaced, "");
+
+	var minValue = parseFloat(this.dataset.min);
+	var maxValue = parseFloat(this.dataset.max);
+	var val = parseFloat(separator == "." ? this.value : this.value.replace(new RegExp(separator, "g"), "."));
+	if (minValue <= maxValue) {
+		if (this.value[0] == "-") {
+			if (this.value.length > 8) 
+				this.value = this.value.substr(0, 8);
+		} else {
+			if (this.value.length > 7) 
+				this.value = this.value.substr(0, 7); 
 		}
+		
+		if (this.value[0] == separator) {
+			this.value = "0" + this.value;
+		}
+		
+		if (minValue < 0 && maxValue < 0) {
+			if (this.value[0] != "-")
+				this.value = "-" + this.value[0];
+		} else if (minValue >= 0 && maxValue >= 0) {
+			if (this.value[0] == "-") 
+				this.value = this.value.substr(0, 0);
+		}
+			 
+		if (val < minValue || val > maxValue)
+			this.value = this.value.substr(0, 0);
+		
+		if (this.value.match(regex)) 
+			if (this.value.match(regex).length > 1) 
+				this.value = this.value.substr(0, 0);
+		
+		if (this.value.match(/\-/g)) 
+			if (this.value.match(/\-/g).length > 1) 
+				this.value = this.value.substr(0, 0);
 	}
-	textwindow.innerHTML = jokes[number];
 }
+
+document.querySelector(".number1").onkeyup = onlyDigits;
+document.querySelector(".number2").onkeyup = onlyDigits;
+
+var inputs = document.getElementsByClassName("input-checkbox");
+for (var i = 0; i < inputs.length; i++) inputs[i].onchange = checkboxHandler;
+         
+function checkboxHandler() {
+    for (var i = 0; i < inputs.length; i++)
+        if (inputs[i].checked && inputs[i] !== this) inputs[i].checked = false;
+}
+
+document.querySelector("#forsubmit").onkeyup = startPHP;
+
+function onAnswer(res) {
+	$('.button-form').attr('disabled', false);
+	var data = JSON.parse(JSON.stringify(res));
+	var result = "<b>Проверка точки (" + data.x + "; " + data.y + ")</b><br>";
+	result += "<b>Параметр: </b>" + data.r + "<br>";
+	result += "<b>Время отправки: </b>" + data.currentTime + "<br>";
+	result += "<b>Время исполнения: </b>" + (parseFloat(data.scriptTime)*1000).toFixed(2) + " ms<br>";
+	result += "<b>Результат: </b>" + data.hit;
+	textwindow.innerHTML = result;
+}
+
+function startPHP() {
+	var x = xtextinput.value;
+	var y = ytextinput.value;
+	var r = false;
+	if (rcheckbox1.checked) r = "1";
+	if (rcheckbox2.checked) r = "2";
+	if (rcheckbox3.checked) r = "3";
+	if (rcheckbox4.checked) r = "4";
+	if (rcheckbox5.checked) r = "5";
+	if (x&&y&&r) {
+		$.ajax({
+			type: "GET",
+			url: "scripts/input.php",
+			data: {
+				"x": x,
+				"y": y,
+				"r": r,
+				"time": (new Date()).getTimezoneOffset()
+			},
+			beforeSend: function() {
+				$('.button-form').attr('disabled', 'disabled');
+			},
+			success: onAnswer,
+			dataType: "json"
+		});
+	} 
+	else 
+		alert('Заполните форму до конца!');
+}
+
